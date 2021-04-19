19c580259dbdb34bd7825639d04ec40d877b03a7
Astrovskiy-199 <Astrovskiy.A@gmail.com>
2021-04-19 17:18:48 +0300

    3
    3

--- csrf/script.js
+++ csrf/script.js
@@ -1,9 +1,28 @@
 var drum = document.getElementById("DrumBlock")
 var ButtonScript = document.getElementById("Button")
 var SoSorry = document.getElementById("SoSorryText")
+var form = document.getElementById("form")
 
-ButtonScript.addEventListener("click", function test() {
 
-    drum.style.transform="Rotate(1440deg)"; SoSorry.style.marginTop="30px";  
-})
 
+
+
+
+  
+
+    ButtonScript.addEventListener("click", function(){
+        
+            drum.style.transform="Rotate(1440deg)"; SoSorry.style.marginTop="30px"; SoSorry.style.animation="none"
+            
+            
+        
+    });
+    
+
+   
+    
+
+    
+  
+
+  
